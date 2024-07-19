85b52537eccdb93c8b89e9861ca841e462f1bfbd
Roberto Leytes <44238463+roberellino@users.noreply.github.com>
2024-07-19 12:15:58 -0300

    Update firebaseAdmin.js

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -4,7 +4,7 @@ const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://console.firebase.google.com/project/appmuedaz/firestore/databases/-default-/data/~2F?hl=es"
+  //databaseURL: "https://console.firebase.google.com/project/appmuedaz/firestore/databases/-default-/data/~2F?hl=es"
 });
 
 const db = admin.firestore();
